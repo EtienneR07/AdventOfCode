7899e0810fc42de8df63c82b574c0a23b224fecb
Etienne <etienner07@gmail.com>
2022-12-10 00:59:51 -0500

    Day 1 and Day 2

---
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Day1.Input.txt";
+            var calories = new List<int>();
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string line;
+                var currentTotalCalories = 0;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (int.TryParse(line, out int food))
+                    {
+                        currentTotalCalories += food;
+                    }
+
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        calories.Add(currentTotalCalories);
+                        currentTotalCalories = 0;
+                    }
+                }
+            }
+
+            var max = calories.Max();
+            var top3 = calories.OrderByDescending(c => c).Take(3).Sum(c => c);
+
+            Console.WriteLine($"Calories of top elf: {max}");
+            Console.WriteLine($"Total for top 3: {top3}");
+        }
+    }
+}
---
+++ AdventOfCode/Day2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Reflection;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static Dictionary<char, (int ForShape, int ForRound)> BasePoints 
+            = new Dictionary<char, (int ForShape, int ForRound)>() { 
+            { 'X', (1, 0) },
+            { 'Y', (2, 3) },
+            { 'Z', (3, 6) }
+        };
+
+        static Dictionary<char, (char Win, char Lost)> ResultsPosibilitiesA
+            = new Dictionary<char, (char Win, char Lost)>() {
+            {'Y', ('A', 'C') },
+            {'X', ('C', 'B') },
+            {'Z', ('B', 'A') },
+        };
+
+        static Dictionary<char, (int Win, int Lost, int Draw)> ResultsPosibilitiesB
+            = new Dictionary<char, (int Win, int Lost, int Draw)>() {
+            {'A', (2, 3, 1) },
+            {'B', (3, 1, 2) },
+            {'C', (1, 2, 3) },
+        };
+
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Day2.Input.txt";
+            var totalPointsPartA = 0;
+            var totalPointsPartB = 0;
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    char[] characters = line.ToCharArray();
+                    totalPointsPartA += GetPointsPartA(characters[2], characters[0]);
+                    totalPointsPartB += GetPointsPartB(characters[2], characters[0]);
+                }
+            }
+
+            Console.WriteLine($"Points part A: {totalPointsPartA}");
+            Console.WriteLine($"Points part B: {totalPointsPartB}");
+        }
+
+        public static int GetPointsPartA(char you, char opponent)
+        {
+            var points = BasePoints[you].ForShape;
+            var rules = ResultsPosibilitiesA[you];
+            if (rules.Win == opponent)
+            {
+                return points += 6;
+            }
+            else if (rules.Lost == opponent)
+            {
+                return points;
+            }
+
+            return points += 3;
+        }
+
+        public static int GetPointsPartB(char you, char opponent)
+        {
+            var points = BasePoints[you].ForRound;
+            var choice = ResultsPosibilitiesB[opponent];
+            if (points == 0)
+            {
+                return points += choice.Lost;
+            }
+            else if (points == 3)
+            {
+                return points += choice.Draw;
+            }
+
+            return points += choice.Win;
+        }
+    }
+}
