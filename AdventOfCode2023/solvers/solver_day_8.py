d9d07b1c50791ebc53aef6cb4b7f851065a3b049
Etienne <etienner07@gmail.com>
2023-12-08 22:28:04 -0500

    Day 8 B

--- AdventOfCode2023/solvers/solver_day_8.py
+++ AdventOfCode2023/solvers/solver_day_8.py
@@ -1,3 +1,5 @@
+from math import lcm
+
 
 class Solver8():
     def get_map_dictionary(self, lines):
@@ -20,37 +22,34 @@ class Solver8():
             return current_map[0]
         return current_map[1]
 
+    def solve_core(self, current_key, data_dict, instruction_line, comparer_func):
+        step = 0
+        zzz_found = False
+        while not zzz_found:
+            for instruction in instruction_line:
+                step += 1
+                current_key = self.get_key_from_instruction(
+                    data_dict[current_key], instruction)
+                if comparer_func(current_key):
+                    zzz_found = True
+                    break
+        return step
+
     def solve_a(self, lines):
         data_dict = self.get_map_dictionary(lines)
         current_key = "AAA"
         instruction_line = lines[0].replace('\n', '')
-        zzz_found = False
-        instruction_count = 0
-        while not zzz_found:
-            for instruction in instruction_line:
-                instruction_count += 1
-                current_key = self.get_key_from_instruction(
-                    data_dict[current_key], instruction)
-                if current_key == 'ZZZ':
-                    zzz_found = True
-                    break
-
-        return instruction_count
+        return self.solve_core(current_key, data_dict, instruction_line, lambda x: x == 'ZZZ')
 
     def solve_b(self, lines):
         data_dict = self.get_map_dictionary(lines)
         current_keys = self.get_starting_keys(data_dict)
         instruction_line = lines[0].replace('\n', '')
-        __z_found = False
-        instruction_count = 0
-        while not __z_found:
-            for instruction in instruction_line:
-                instruction_count += 1
-                current_keys = [self.get_key_from_instruction(
-                    data_dict[x], instruction) for x in current_keys]
 
-                if all(x[-1] == 'Z' for x in current_keys):
-                    __z_found = True
-                    break
+        all_steps = []
+        for current_key in current_keys:
+            all_steps.append(self.solve_core(
+                current_key, data_dict, instruction_line, lambda x: x[-1] == 'Z'))
 
-        return instruction_count
+        print(*all_steps)
+        return lcm(*all_steps)
