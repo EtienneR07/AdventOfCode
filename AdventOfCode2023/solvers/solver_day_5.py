43da1f20380dc66dc480c954fa88e997c87b0031
Etienne <etienner07@gmail.com>
2023-12-09 18:26:09 -0500

    Day9

--- AdventOfCode2023/solvers/solver_day_5.py
+++ AdventOfCode2023/solvers/solver_day_5.py
@@ -1,42 +1,11 @@
-import copy
-
-
 class Solver5():
     def feed_seeds(self, lines):
         return [[int(s)] for s in lines[0].split(':')[1].split()]
 
-    def merge_ranges(self, ranges):
-        if not ranges:
-            return []
-
-        if len(ranges) == 1:
-            return ranges
-
-        sorted_ranges = sorted(ranges, key=lambda x: x[0])
-
-        merged_ranges = [sorted_ranges[0]]
-
-        for current_start, current_end in sorted_ranges[1:]:
-            previous_start, previous_end = merged_ranges[-1]
-
-        if current_start <= previous_end:
-            merged_ranges[-1] = (previous_start,
-                                 max(current_end, previous_end))
-        else:
-            merged_ranges.append((current_start, current_end))
-
-        return merged_ranges
-
     def feed_seed_ranges(self, lines):
         numbers = lines[0].split(':')[1].split()
-        seed_ranges = [(int(numbers[i]), int(numbers[i]) + int(numbers[i + 1]))
-                       for i in range(0, len(numbers) - 1, 2)]
-        seed_ranges = []
-        for seed_range in seed_ranges:
-            seed_ranges.append([[seed_range]])
-
-        print(seed_ranges)
-        return seed_ranges
+        return [([range(int(numbers[i]), int(numbers[i]) + int(numbers[i + 1]))])
+                for i in range(0, len(numbers) - 1, 2)]
 
     def in_range(self, number, range):
         return number >= range[0] and number <= range[1]
@@ -45,35 +14,12 @@ class Solver5():
         numbers = [int(n) for n in line.split()]
         return (numbers[1], numbers[1] + numbers[2], numbers[0])
 
-    def get_intersection(self, rangeA, rangeB):
-        start_intersection = max(rangeA[0], rangeB[0])
-        end_intersection = min(rangeA[1], rangeB[1])
-        if start_intersection <= end_intersection:
-            return (start_intersection, end_intersection)
-        else:
-            return None
+    def get_real_ranges(self, line):
+        numbers = [int(n) for n in line.split()]
+        return (range(numbers[1], numbers[1] + numbers[2]), numbers[0])
 
-    def retract_intersection(self, rangeA, range_to_retract):
-        intersection = self.get_intersection(rangeA, range_to_retract)
-        if intersection is None:
-            return [rangeA]
-
-        startA, endA = rangeA
-        startIntersection, endIntersection = intersection
-
-        if startA < startIntersection and endIntersection < endA:
-            return [(startA, startIntersection - 1), (endIntersection + 1, endA)]
-        else:
-            return [(startA, intersection[0] - 1)] if (intersection[0] < endA and intersection[0] > startA) else [(intersection[1] + 1, endA)]
-
-    def process(self, ranges, mapper):
-        if all(isinstance(sublist, list) and all(isinstance(item, int) for item in sublist) for sublist in mapper):
-            self.process_ranges_for_seeds(ranges, mapper)
-            return
-        self.process_ranges_for_seed_ranges(ranges, mapper)
-
-    def process_ranges_for_seeds(self, ranges, seeds_mapper):
-        for index, current_level in enumerate(seeds_mapper):
+    def process_ranges(self, ranges, seed_mapper):
+        for index, current_level in enumerate(seed_mapper):
             number_to_map = current_level[-1]
             found = False
             for range in ranges:
@@ -82,17 +28,14 @@ class Solver5():
 
                 if self.in_range(current_level[-1], range):
                     distance = number_to_map - range[0]
-                    seeds_mapper[index].append(range[2] + distance)
+                    seed_mapper[index].append(range[2] + distance)
                     found = True
 
             if not found:
-                seeds_mapper[index].append(number_to_map)
+                seed_mapper[index].append(number_to_map)
 
-    def process_ranges_for_seed_ranges(self, ranges, seed_ranges_mapper):
-        for index, current_level in enumerate(seed_ranges_mapper):
-            return 0
-
-    def solve_core(self, lines, mapping):
+    def solve_a(self, lines):
+        seed_mapper = self.feed_seeds(lines)
         ranges_to_process = []
         for line in lines[1:]:
             if line.strip() == "":
@@ -100,26 +43,47 @@ class Solver5():
 
             if any(char.isalpha() for char in line):
                 if any(ranges_to_process):
-                    self.process(ranges_to_process, mapping)
+                    self.process_ranges(ranges_to_process, seed_mapper)
                     ranges_to_process = []
                 continue
 
             ranges_to_process.append(self.get_ranges(line))
 
         if any(ranges_to_process):
-            self.process(ranges_to_process, mapping)
+            self.process_ranges(ranges_to_process, seed_mapper)
 
-        locations = [i[-1] for i in mapping]
+        locations = [i[-1] for i in seed_mapper]
 
-        if all(isinstance(sublist, list) and all(isinstance(item, int) for item in sublist) for sublist in mapping):
-            return min(locations)
-
-        print(mapping)
-
-    def solve_a(self, lines):
-        seeds_mapper = self.feed_seeds(lines)
-        return self.solve_core(lines, seeds_mapper)
+        return min(locations)
 
     def solve_b(self, lines):
-        seed_ranges_mapper = self.feed_seed_ranges(lines)
-        return self.solve_core(lines, seed_ranges_mapper)
+        seed_ranges = self.feed_seed_ranges(lines)
+        ranges_to_process = []
+        for line in lines[1:]:
+            if line.strip() == "":
+                continue
+
+            if any(char.isalpha() for char in line):
+                if any(ranges_to_process):
+                    self.process_b(ranges_to_process, seed_ranges)
+                    ranges_to_process = []
+                continue
+
+            ranges_to_process.append(self.get_real_ranges(line))
+
+            if any(ranges_to_process):
+                self.process_b(ranges_to_process, seed_ranges)
+
+        return 0
+
+    def process_b(self, ranges_to_process, seed_ranges):
+        for seed_range in seed_ranges:
+            for to_process in ranges_to_process:
+                for current_seed_range in seed_range:
+                    current_seed_range_set = set(current_seed_range)
+                    to_process_set = set(to_process[0])
+                    intersection_set = current_seed_range_set.intersection(
+                        to_process_set)
+                    print(intersection_set)
+
+        return 0
--- AdventOfCode2023/solvers/solver_day_9.py
+++ AdventOfCode2023/solvers/solver_day_9.py
@@ -1,8 +1,32 @@
 
 class Solver9():
+    def solve_core(self, lines, steps_lambda):
+        result = 0
+        for line in lines:
+            steps_levels = [[int(n) for n in line.split()]]
+
+            all_zero_steps = False
+            while not all_zero_steps:
+                previous = None
+                new_level = []
+                for number in steps_levels[-1]:
+                    if previous != None:
+                        new_level.append(number - previous)
+                    previous = number
+
+                if len(new_level) > 0 and all(n == 0 for n in new_level):
+                    all_zero_steps = True
+                steps_levels.append(new_level)
+
+            current_step = 0
+            for level in reversed(steps_levels[:-1]):
+                current_step = steps_lambda(level, current_step)
+
+            result += current_step
+        return result
 
     def solve_a(self, lines):
-        return 0
+        return self.solve_core(lines, lambda x, y: x[-1] + y)
 
     def solve_b(self, lines):
-        return 0
+        return self.solve_core(lines, lambda x, y: x[0] - y)
