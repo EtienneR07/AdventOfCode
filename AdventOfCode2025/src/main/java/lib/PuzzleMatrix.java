49c48f5d8f8612be1e7173392954d59aa6e4df06
Etienne <etienner07@gmail.com>
2025-10-13 13:45:38 -0400

    implement getNeighbors

--- AdventOfCode2025/src/main/java/lib/PuzzleMatrix.java
+++ AdventOfCode2025/src/main/java/lib/PuzzleMatrix.java
@@ -15,12 +15,56 @@ public abstract class PuzzleMatrix<T> {
         return matrix.getFirst().size();
     }
 
-    public Cell<T> getValue(int row, int col) {
-        return matrix.get(row).get(col);
+    public Cell<T> getValue(int x, int y) {
+        return matrix.get(y).get(x);
     }
 
-    public boolean inBounds(int row, int col) {
-        return row >= 0 && row < height() && col >= 0 && col < width();
+    public boolean inBounds(int x, int y) {
+        return y >= 0 && y < height() && x >= 0 && x < width();
+    }
+
+    public ArrayList<Cell<T>> diagonalDownRight(int x, int y) {
+        var diagonalList = new ArrayList<Cell<T>>();
+
+        var row = y;
+        var col = x;
+        while (inBounds(row, col)) {
+            diagonalList.add(getValue(row, col));
+            row++;
+            col++;
+        }
+
+        return diagonalList;
+    }
+
+    public ArrayList<Cell<T>> diagonalDownLeft(int x, int y) {
+        var diagonalList = new ArrayList<Cell<T>>();
+
+        var row = y;
+        var col = x;
+        while (inBounds(row, col)) {
+            diagonalList.add(getValue(row, col));
+            row++;
+            col--;
+        }
+
+        return diagonalList;
+    }
+
+    public ArrayList<Cell<T>> getNeighbors(int x, int y) {
+        var neighbors = new ArrayList<Cell<T>>();
+        if (!inBounds(x, y)) return neighbors;
+        
+        for (int row = y - 1; row < y + 2; row++) {
+            for (int col = x - 1; col < x + 2; col++) {
+                if (x == col && row == y) continue;
+                if (!inBounds(col, row)) continue;
+
+                neighbors.add(getValue(col, row));
+            }
+        }
+
+        return neighbors;
     }
 
     public void print() {
